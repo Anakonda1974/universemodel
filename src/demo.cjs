c5210e101fc14c01246414588a9979b6b325770f
Anakonda1974 <sebastian.stevens19@googlemail.com>
2025-06-27 02:17:58 +0200

    GO

---
+++ src/demo.cjs
@@ -0,0 +1,9 @@
+
+
+
+import { SeedManager } from "./dist/SeedManager";
+import { PropertyGraph, PropertyDefinition } from "./dist/PropertyGraph";
+import { ProceduralEntity } from "./dist/ProceduralEntity";
+import { getNoise01, mapRange01 } from "./dist/ProceduralUtils";
+
+const seedManager = new SeedManager("GenesisAlpha42");
